20f9d7f9ef36012a0f63653fe4c33717ae665c2d
JhonathanPb <jhonathan.pablo.ss321@gmail.com>
2025-08-21 13:16:00 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace MiniRPG
 {
@@ -10,22 +11,44 @@ namespace MiniRPG
         {
             // Inicializar os dados do jogo
             DadosDoJogo.CarregadorDados();
-            
-            // Menu para exibir o número de heróis e inimigos disponíveis
-            Console.WriteLine("-----SELELCIONE UM HEROI-----");
-            Console.WriteLine();
-            for (int h = 0; h < DadosDoJogo.heroisdisponiveis.Count; h++)
+
+            Heroi heroiSelecionado = null;
+            bool escolhaInvalida = true;
+
+            while (escolhaInvalida)
             {
-                Heroi heroi = DadosDoJogo.heroisdisponiveis[h];
-                Console.WriteLine($"{h + 1}. Nome: {heroi.Nome} - Classe: {heroi.Classe}");
-            }
+                // Menu para exibir o número de heróis e inimigos disponíveis
+                Console.WriteLine("-----SELELCIONE UM HEROI-----");
+                Console.WriteLine();
+                for (int h = 0; h < DadosDoJogo.heroisdisponiveis.Count; h++)
+                {
+                    Heroi heroi = DadosDoJogo.heroisdisponiveis[h];
+                    Console.WriteLine($"{h + 1}. Nome: {heroi.Nome} - Classe: {heroi.Classe}");
+                }
 
-            // Escolha do Heroi de 1 a 4
-            Console.WriteLine("\nEscolha um Heroi: ")
-            string escolha = Console.ReadLine()
-
-
+                // Escolha do Heroi de 1 a 4
+                Console.WriteLine("\nEscolha um Heroi: ");
+                string opcao = Console.ReadLine();
+                int escolha;
+                
+                if (int.TryParse(opcao, out escolha) && escolha > 0 && escolha <= DadosDoJogo.heroisdisponiveis.Count)
+                {
+                    heroiSelecionado = DadosDoJogo.heroisdisponiveis[escolha - 1];
+                    Thread.Sleep(4000);
+                    Console.Clear();
+                    Console.WriteLine($"Herois Escolhido: {heroiSelecionado.Nome}");
+                    escolhaInvalida = false;
+                }
+                else
+                {
+                    Console.WriteLine("\nEntrada Inválida. Digite APENAS NÚMEROS!");
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                    escolhaInvalida = true;
+                }
             
+            } 
+        
         }
 
     }
