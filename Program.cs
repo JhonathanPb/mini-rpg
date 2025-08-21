066163014e7244904942611addece5319bdbc4b9
JhonathanPb <jhonathan.pablo.ss321@gmail.com>
2025-08-21 12:32:56 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,10 +12,18 @@ namespace MiniRPG
             DadosDoJogo.CarregadorDados();
             
             // Menu para exibir o número de heróis e inimigos disponíveis
-            Console.WriteLine("-----SELELCIONE UM HEROI-----")
-            Console.WriteLine()
-            Console.WriteLine(DadosDoJogo.heroisdisponiveis.Nome)
-            break
+            Console.WriteLine("-----SELELCIONE UM HEROI-----");
+            Console.WriteLine();
+            for (int h = 0; h < DadosDoJogo.heroisdisponiveis.Count; h++)
+            {
+                Heroi heroi = DadosDoJogo.heroisdisponiveis[h];
+                Console.WriteLine($"{h + 1}. Nome: {heroi.Nome} - Classe: {heroi.Classe}");
+            }
+
+            // Escolha do Heroi de 1 a 4
+            Console.WriteLine("\nEscolha um Heroi: ")
+            string escolha = Console.ReadLine()
+
 
             
         }
